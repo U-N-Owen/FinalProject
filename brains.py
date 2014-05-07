73df4fafe777a8a5114b96a40d7c1700d94d24fd
gitful <arthur.migdal@gmail.com>
2014-05-07 14:57:20 +0000

    updated pseudocode with a bit more real code, started the brains for mastermind

---
+++ brains.py
@@ -0,0 +1,10 @@
+'''
+The brains for the MasterMind.py program
+This file includes a class for one game.
+'''
+
+class mastermindGame(object):
+	def __init__(self, colors, rows):
+		self.__colors	= int(colors)
+		self.__rows		= int(rows)
+#		self.__
