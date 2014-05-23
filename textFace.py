a343b2b955f2d844c80acbfff63c9314851ee254
gitful <arthur.migdal@gmail.com>
2014-05-22 20:11:02 -0400

    A bit more. Needs to be better and such. MEEEEEEP.

--- textFace.py
+++ textFace.py
@@ -23,14 +23,34 @@ def numInput(message, error=""):
 	else:
 		return num
 
+def header(turn, turns, colors):
+	x = len(str(turns)) - len(str(turn))
+	y = ""
+	for i in range(0, x):
+		y += " "
+	stdscr.addstr(0, 0, "You have " + str(turn) + " turns left out of " + str(turns) + " total. You have a total of " + str(colors) + " colors.", curses.A_REVERSE)
+	stdscr.addstr(y)
+
 turns = numInput("How many turns do you want to have?")
+turn = turns
 colors = numInput("How many colors do you want to have?")
+while colors < 2:
+	colors = numInput("How many colors do you want to have?", "INVALID INPUT")
 
 stdscr.clear()
 
+header(turn, turns, colors)
 
+nextY = 2
 
-while 1:
+# set up the "template" for stuff
+temp = " " * len(str(colors-1))
+template = "| " + temp + ", " + temp + ", " + temp + ", " + " |   |   |"
+del temp
+# and shove up a nice line onto the screen :D
+stdscr.addstr(1, 0, "-" * len(template))
+
+#while turn != 0 and :
 	
 
 curses.endwin()
